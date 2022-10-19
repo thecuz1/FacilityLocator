bb15e0a1292c0b71b0317029de3bcf95fc6763da
thecuz1 <ccpc54@hotmail.com>
2022-10-18 23:00:08 -0230

    Remove select class

--- extensions/modify.py
+++ extensions/modify.py
@@ -8,22 +8,19 @@ services = ['Bcons', 'Pcons & Pipes', 'Scons', 'Oil & Petrol', 'Heavy Oil', 'Enr
             'Light Assembly (Motor Pool)', 'Light Assembly (Rocket Factory)', 'Light Assembly (Field Station)', 'Light Assembly (Tank Factory)', 'Light Assembly (Weapons Platform)', 'Modification Center', 'Ammo Factory', 'Ammo Factory (Rocket)', 'Ammo Factory (Large Shell)', 'Large Assembly', 'Large Assembly (Train)', 'Large Assembly (Heavy Tank)']
 
 
-class ServicesSelectMenu(discord.ui.Select):
-    def __init__(self):
-        options = []
-        for service in services:
-            options.append(discord.SelectOption(label=service))
-        super().__init__(max_values=len(options), options=options)
-
-    async def callback(self, interaction: discord.Interaction):
-        await interaction.response.defer()
-
-
 class ServicesSelectView(discord.ui.View):
+    options = []
+    for service in services:
+        options.append(discord.SelectOption(label=service))
+
     async def on_timeout(self) -> None:
         for item in self.children:
             item.disabled = True
         await self.message.edit(view=self)
+
+    @discord.ui.select(options=options, max_values=len(services))
+    async def select_menu(self, interaction: discord.Interaction, select_menu: discord.ui.Select):
+        await interaction.response.defer()
 
     @discord.ui.button(label='Finish', style=discord.ButtonStyle.success, row=4)
     async def finish(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -34,9 +31,9 @@ class Modify(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @ app_commands.command()
-    @ app_commands.autocomplete(region=region_autocomplete)
-    @ app_commands.rename(facilityname='facility-name', region='region-hex', coordinates='coordinates', maintainer='maintainer', notes='notes')
+    @app_commands.command()
+    @app_commands.autocomplete(region=region_autocomplete)
+    @app_commands.rename(facilityname='facility-name', region='region-hex', coordinates='coordinates', maintainer='maintainer', notes='notes')
     async def create(self, interaction: discord.Interaction, facilityname: str, region: str, coordinates: str, maintainer: str, notes: str):
         e = discord.Embed(
             title=facilityname, description=notes, color=0x54A24A)
@@ -47,8 +44,6 @@ class Modify(commands.Cog):
         e.add_field(name='Author', value=interaction.user.mention)
 
         view = ServicesSelectView()
-        select = ServicesSelectMenu()
-        view.add_item(select)
 
         await interaction.response.send_message('Select services below', embed=e, view=view, ephemeral=True)
         view.message = await interaction.original_response()

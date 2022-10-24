fc9c5a061f003e953d75acf2fbd4965591213ae3
thecuz1 <ccpc54@hotmail.com>
2022-10-23 23:41:01 -0230

    Added information modal

--- cogs/modify.py
+++ cogs/modify.py
@@ -5,6 +5,27 @@ from utils.autocomplete import region_autocomplete
 from utils.enums import Service
 
 
+class FacilityInformationModal(discord.ui.Modal, title='Edit Facility Information'):
+    facility_name = discord.ui.TextInput(label='Facility Name')
+    maintainer = discord.ui.TextInput(label='Maintainer')
+    notes = discord.ui.TextInput(label='Notes',
+                                 style=discord.TextStyle.paragraph,
+                                 required=False)
+
+    def __init__(self, facility_name, maintainer, notes):
+        super().__init__()
+        self.facility_name.default = facility_name
+        self.maintainer.default = maintainer
+        self.notes.default = notes
+
+    async def on_submit(self, interaction: discord.Interaction):
+        self.submitted = (str(self.facility_name),
+                          str(self.maintainer),
+                          str(self.notes))
+        self.last_interaction = interaction
+        self.stop()
+
+
 class ServicesSelectView(discord.ui.View):
     options = []
     for service in Service:
@@ -19,6 +40,22 @@ class ServicesSelectView(discord.ui.View):
     @discord.ui.select(options=options, max_values=len(Service), placeholder='Select services')
     async def select_menu(self, interaction: discord.Interaction, select_menu: discord.ui.Select):
         await interaction.response.defer()
+
+    @discord.ui.button(label='Add Description/Edit')
+    async def edit(self, interaction: discord.Interaction, button: discord.ui.Button):
+        information = FacilityInformationModal(
+            facility_name=self.facility_name,
+            maintainer=self.maintainer,
+            notes=self.notes)
+        await interaction.response.send_modal(information)
+        if await information.wait():
+            return
+        self.facility_name, self.maintainer, self.notes = information.submitted
+        embed = interaction.message.embeds[0]
+        embed.description = self.notes
+        embed.title = self.facility_name
+        embed.set_field_at(1, name='Maintainer', value=self.maintainer)
+        await information.last_interaction.response.edit_message(embed=embed)
 
     @discord.ui.button(label='Finish', style=discord.ButtonStyle.primary)
     async def finish(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -41,25 +78,30 @@ class Modify(commands.Cog):
 
     @app_commands.command()
     @app_commands.autocomplete(region=region_autocomplete)
-    @app_commands.rename(facilityname='facility-name', region='region-coordinates', maintainer='maintainer', notes='notes')
-    async def create(self, interaction: discord.Interaction, facilityname: str, region: str, maintainer: str, notes: str):
+    @app_commands.rename(facility_name='facility-name', region='region-coordinates', maintainer='maintainer')
+    async def create(self, interaction: discord.Interaction, facility_name: str, region: str, maintainer: str):
         author = interaction.user
-        e = discord.Embed(title=facilityname,
-                          description=notes,
+        e = discord.Embed(title=facility_name,
                           color=0x54A24A)
         e.add_field(name='Region-Coordinates', value=region)
         e.add_field(name='Maintainer', value=maintainer)
         e.add_field(name='Author', value=author.mention)
 
         view = ServicesSelectView()
+        view.facility_name = facility_name
+        view.maintainer = maintainer
+        view.notes = ''
 
         await interaction.response.send_message(embed=e, view=view, ephemeral=True)
         view.message = await interaction.original_response()
 
         if await view.wait():
             return
+        facility_name = view.facility_name
+        maintainer = view.maintainer
+        notes = view.notes
         await self.bot.db.add_facility(
-            facilityname, region, maintainer, view.flag_number, notes, author.id)
+            facility_name, region, maintainer, view.flag_number, notes, author.id)
         await view.last_interaction.response.send_message(':white_check_mark: Successfully added facility', ephemeral=True)
 
 

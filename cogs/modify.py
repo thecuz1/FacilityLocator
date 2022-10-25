342f915fde7d4a021f315af4991eb49b5b05d3cc
thecuz1 <ccpc54@hotmail.com>
2022-10-25 03:56:44 -0230

    Added facility dataclass

--- cogs/modify.py
+++ cogs/modify.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from copy import deepcopy
 import discord
 from discord.ext import commands
@@ -6,23 +7,34 @@ from utils.autocomplete import region_autocomplete
 from utils.enums import Service
 
 
+@dataclass
+class Facility:
+    name: str
+    region: str
+    maintainer: str
+    services: int = 0
+    description: str = ''
+
+
 class FacilityInformationModal(discord.ui.Modal, title='Edit Facility Information'):
-    facility_name = discord.ui.TextInput(label='Facility Name')
+    name = discord.ui.TextInput(label='Facility Name')
     maintainer = discord.ui.TextInput(label='Maintainer')
     description = discord.ui.TextInput(label='Description',
                                        style=discord.TextStyle.paragraph,
                                        required=False)
 
-    def __init__(self, facility_name, maintainer, description):
+    def __init__(self, facility):
         super().__init__()
-        self.facility_name.default = facility_name
-        self.maintainer.default = maintainer
-        self.description.default = description
+        self.facility = facility
+        self.name.default = facility.name
+        self.maintainer.default = facility.maintainer
+        self.description.default = facility.description
 
     async def on_submit(self, interaction: discord.Interaction):
-        self.submitted = (str(self.facility_name),
-                          str(self.maintainer),
-                          str(self.description))
+        self.facility.name = str(self.name)
+        self.facility.maintainer = str(self.maintainer)
+        self.facility.description = str(self.description)
+
         self.last_interaction = interaction
         self.stop()
 
@@ -47,18 +59,17 @@ class ServicesSelectView(discord.ui.View):
 
     @discord.ui.button(label='Add Description/Edit')
     async def edit(self, interaction: discord.Interaction, button: discord.ui.Button):
-        information = FacilityInformationModal(
-            facility_name=self.facility_name,
-            maintainer=self.maintainer,
-            description=self.description)
+        information = FacilityInformationModal(self.facility)
         await interaction.response.send_modal(information)
+
         if await information.wait():
             return
-        self.facility_name, self.maintainer, self.description = information.submitted
         embed = interaction.message.embeds[0]
-        embed.description = self.description
-        embed.title = self.facility_name
-        embed.set_field_at(1, name='Maintainer', value=self.maintainer)
+        embed.description = self.facility.description
+        embed.title = self.facility.name
+        embed.set_field_at(1,
+                           name='Maintainer',
+                           value=self.facility.maintainer)
         await information.last_interaction.response.edit_message(embed=embed)
 
     @discord.ui.button(label='Finish', style=discord.ButtonStyle.primary)
@@ -67,15 +78,14 @@ class ServicesSelectView(discord.ui.View):
         if not len(selected_services) > 0:
             return await interaction.response.send_message('⚠️ Please select at least one service', ephemeral=True)
 
-        flag_number = 0
         for service in selected_services:
             service = Service[service]
-            flag_number += service.value[0]
-        self.flag_number = flag_number
-        self.last_interaction = interaction
+            self.facility.services += service.value[0]
+
         for item in self.children:
             item.disabled = True
         await interaction.response.edit_message(view=self)
+        self.followup = interaction.followup
         self.stop()
 
 
@@ -89,32 +99,28 @@ class Modify(commands.Cog):
     @app_commands.rename(facility_name='facility-name', region='region-coordinates', maintainer='maintainer')
     async def create(self, interaction: discord.Interaction, facility_name: str, region: str, maintainer: str):
         author = interaction.user
-        e = discord.Embed(title=facility_name,
-                          color=0x54A24A)
-        e.add_field(name='Region-Coordinates', value=region)
-        e.add_field(name='Maintainer', value=maintainer)
-        e.add_field(name='Author', value=author.mention)
+        embed = discord.Embed(title=facility_name,
+                              color=0x54A24A)
+        embed.add_field(name='Region-Coordinates', value=region)
+        embed.add_field(name='Maintainer', value=maintainer)
+        embed.add_field(name='Author', value=author.mention)
 
         view = ServicesSelectView()
-        view.facility_name = facility_name
-        view.maintainer = maintainer
-        view.description = ''
+        view.facility = Facility(facility_name, region, maintainer)
 
-        await interaction.response.send_message(embed=e, view=view, ephemeral=True)
+        await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
         view.message = await interaction.original_response()
 
         if await view.wait():
             return
-        facility_name = view.facility_name
-        maintainer = view.maintainer
-        description = view.description
+        facility = view.facility
         try:
-            await self.bot.db.add_facility(facility_name, region, maintainer, view.flag_number, description, author.id)
+            await self.bot.db.add_facility(facility.name, facility.region, facility.maintainer, facility.services, facility.description, author.id)
         except Exception as e:
-            await view.last_interaction.followup.send(':x: Failed to add facility', ephemeral=True)
+            await view.followup.send(':x: Failed to add facility', ephemeral=True)
             raise e
         else:
-            await view.last_interaction.followup.send(':white_check_mark: Successfully added facility', ephemeral=True)
+            await view.followup.send(':white_check_mark: Successfully added facility', ephemeral=True)
 
 
 async def setup(bot: commands.bot) -> None:

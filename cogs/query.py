38fc097bf5aa40da04e0a041cbbf5d0fc1517b14
thecuz1 <ccpc54@hotmail.com>
2023-01-20 18:47:54 -0330

    Send multiple embeds in list command

--- cogs/query.py
+++ cogs/query.py
@@ -1,4 +1,4 @@
-from itertools import groupby
+from itertools import groupby, combinations
 import re
 import rapidfuzz
 from discord import Interaction, Embed, Member, Colour
@@ -184,21 +184,37 @@ class Query(commands.Cog):
         facility_list.sort(key=lambda facility: facility.region)
         facility_regions = groupby(facility_list, key=lambda facility: facility.region)
 
+        finished_embeds = []
+
         for region, facilities in facility_regions:
             formatted_list = (
                 f"{facility.id_} | {facility.name} | {facility.marker}\n"
                 for facility in facilities
             )
-            embed.add_field(name=f"{region} (0)", value="")
+            if len(name := f"{region} (0)") + len(embed) + 150 > 6000:
+                finished_embeds.append(embed)
+                embed = Embed()
+                embed.colour = Colour.green()
+            embed.add_field(name=name, value="")
             field_index = len(embed.fields) - 1
 
             for entry in formatted_list:
                 previous_value = embed.fields[field_index].value or ""
-                if len(field_value := previous_value + entry) > 1024:
+                new_value = previous_value + entry
+
+                if len(embed) + len(entry) > 6000 or (
+                    len(new_value) > 1024 and len(embed.fields) == 25
+                ):
+                    finished_embeds.append(embed)
+                    embed = Embed()
+                    embed.colour = Colour.green()
                     embed.add_field(name=f"{region} (1) (Cont.)", value=entry)
-                    field_index = len(embed.fields) - 1
+                    field_index = 0
+                elif len(new_value) > 1024:
+                    embed.add_field(name=f"{region} (1) (Cont.)", value=entry)
+                    field_index += 1
                 else:
-                    facility_count = str(field_value.count("\n"))
+                    facility_count = str(new_value.count("\n"))
                     field_name = (
                         embed.fields[field_index].name or f"{region} ({facility_count})"
                     )
@@ -211,10 +227,16 @@ class Query(commands.Cog):
                     embed.set_field_at(
                         field_index,
                         name=updated_name,
-                        value=field_value,
+                        value=new_value,
                     )
 
-        await interaction.response.send_message(embed=embed)
+        if embed not in finished_embeds:
+            finished_embeds.append(embed)
+
+        await interaction.response.send_message(embed=finished_embeds.pop(0))
+
+        for embed in finished_embeds:
+            await interaction.followup.send(embed=embed)
 
 
 async def setup(bot: commands.Bot) -> None:
